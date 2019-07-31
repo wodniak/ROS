cc95cd970e23abcb1e0320b7772930f7345f46f7
gwozniak <wozgrz96@gmail.com>
2019-07-31 18:11:32 -0500

    config with opencv 4.1.1

---
+++ src/ros_webcam/include/webcam.h
@@ -0,0 +1,20 @@
+#include "ros/ros.h"
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <sstream> // for converting the command line parameter to integer
+
+
+class WebcamImage
+{
+private:
+    ros::NodeHandle nh;
+    image_transport::ImageTransport it;
+    image_transport::Publisher pub;
+
+
+public:
+    WebcamImage(int videoSource);
+    ~WebcamImage();
+
+};
---
+++ src/ros_webcam/src/webcam.cpp
@@ -0,0 +1,63 @@
+#include "webcam.h"
+
+
+WebcamImage::WebcamImage(int videoSource) : nh(), it(nh)
+{
+    image_transport::Publisher pub = it.advertise("camera/image", 1);
+
+    cv::VideoCapture cap(videoSource);
+    // Check if video device can be opened with the given index
+    if(!cap.isOpened())
+    {
+        exit(1);
+    }
+
+    cv::Mat frame;
+    sensor_msgs::ImagePtr msg;
+
+    ros::Rate loop_rate(5);     //Hz
+    while (nh.ok())
+    {
+        cap >> frame;
+        // Check if grabbed frame is actually full with some content
+        if(!frame.empty())
+        {
+            msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+            pub.publish(msg);
+            cv::waitKey(1);
+        }
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+}
+
+
+WebcamImage::~WebcamImage()
+{
+
+}
+
+
+int main(int argc, char** argv)
+{
+    // Check if video source has been passed as a parameter
+    if(argv[1] == NULL)
+    {
+        ROS_INFO("Video source not passed as parameter.");
+        return 1;
+    }
+
+    // Convert the passed argument as command line parameter index for the video device to an integer
+    std::istringstream video_sourceCmd(argv[1]);
+    int videoSource;
+    if(!(video_sourceCmd >> videoSource))
+    {
+        ROS_INFO("Could not convert passed argument to integer.");
+        return 1;
+    }
+
+    ros::init(argc, argv, "webcamImage_publisher");
+    WebcamImage webcam(videoSource);
+
+}
